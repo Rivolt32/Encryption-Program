12aa3a5eae0cd7c2fcfbd644864fe0bb6a83348e
Rivolt32 <tarik.elharim@gmail.com>
2022-04-06 22:39:55 +0000

    Uploading the files
    The first steps of creating the encryption program

---
+++ main.py
@@ -0,0 +1,22 @@
+chars = "cZ08^<qhwstuR,-defgzxBEOPnijklmrop%(+HIY:}~QNTUv>?6=yb!\"#$SFXAM1./2;GLJK CDVaW4&'3)*5@[7`{_9]|\\"
+steps = 2
+
+encresult = []
+decresult = []
+
+#encryption
+text = list(input('Enter a text to encrypt: '))
+for i in text:
+	i = chars[chars.index(i)+steps]
+	encresult.append(i)
+print("After encryption:",''.join(encresult))
+
+#decryption
+for i in encresult:
+	i = chars[chars.index(i)-steps]
+	decresult.append(i)
+print('After decryption:',''.join(decresult))
+
+
+
+input('')
