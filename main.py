6684c5e75b4b27ba5e505327204055174daed8b7
Rivolt32 <tarik.elharim@gmail.com>
2022-04-07 16:01:42 +0000

    removed test changes

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ while True:
 	print("   |1| Encrypt a text")
 	print("   |2| Decrypt a text")
 	print("   |0| Leave the program")
-	task = input("teeesttt ")
+	task = input(" ")
 	
 	#encryption
 	if task == "1":
