19dd33eb57fed0765c277503de52580f47d9329a
Rivolt32 <tarik.elharim@gmail.com>
2022-04-07 01:18:22 +0000

    Big update
    New changes:
    - Made an interface
    - Added the exit option
    - You can now choose between Encrypting a text and decrypting it

--- main.py
+++ main.py
@@ -7,23 +7,45 @@ print("---------------------------------------------------")
 print("| Encryption / Decryption | 07/04/2022 | Rivolt32 |")
 print("---------------------------------------------------")
 print("|           Type 'exit' anytime to leave          |")
+print("---------------------------------------------------\n")
 
 while True:
 	print(" What are you wishing to do")
+	print("   |1| Encrypt a text")
+	print("   |2| Decrypt a text")
+	print("   |0| Leave the program")
+	task = input(" ")
+	
 	#encryption
-	text = list(input('Enter a text to encrypt: '))
-	for i in text:
-		i = chars[chars.index(i)+steps]
-		encresult.append(i)
-	print("After encryption:",''.join(encresult))
+	if task == "1":
+		print("-------------------------------")
+		text = input(' Enter a text to encrypt:\n')
+		if text == "exit":
+			break
+		for i in text:
+			i = chars[chars.index(i)+steps]
+			encresult.append(i)
+		print("Your encrypted text:\n",''.join(encresult))
+		print('-------------------------------\n \n \n')
 
 	#decryption
-	for i in encresult:
-		i = chars[chars.index(i)-steps]
-		decresult.append(i)
-	print('After decryption:',''.join(decresult))
+	if task == "2":
+		print("-------------------------------")
+		text = input(' Enter a text to decrypt:\n')
+		if text == "exit":
+			break
+		for i in text:
+			i = chars[chars.index(i)-steps]
+			decresult.append(i)
+		print('Your decrypted text:\n',''.join(decresult))
+		print('-------------------------------\n \n \n')
 
-	print('--------------------------')
+	#exit
+	if task == "exit" or task == "0":
+		break
+
+	if task != "1" and task != "2":
+		print("Invalid input, choose one of the options available.\n-------------------------------")
 
 	encresult = []
 	decresult = []
