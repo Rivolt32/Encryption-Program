b95e0704d97813b6e81a04dcef5f5b273b5aecb9
Rivolt32 <tarik.elharim@gmail.com>
2022-04-07 16:44:19 +0000

    Added functions
    In this update, i converted the way the program works to functions. Same result as befor, but better organised and will allow me next to use different encryption keys for every encryption / decryption .

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+#functions
+def encrypt():
+	print("-------------------------------")
+	text = input(' Enter a text to encrypt:\n')
+	if text == "exit":
+		return 0
+	for i in text:
+		i = chars[chars.index(i)+steps]
+		encresult.append(i)
+	print("Your encrypted text:\n",''.join(encresult))
+	print('-------------------------------\n \n \n')
+
+
+def decrypt():
+	print("-------------------------------")
+	text = input(' Enter a text to decrypt:\n')
+	if text == "exit":
+		return 0
+	for i in text:
+			i = chars[chars.index(i)-steps]
+			decresult.append(i)
+	print('Your decrypted text:\n',''.join(decresult))
+	print('-------------------------------\n \n \n')
+	
+#end of functions def
+
+
 chars = "cZ08^<qhwstuR,-defgzxBEOPnijklmrop%(+HIY:}~QNTUv>?6=yb!\"#$SFXAM1./2;GLJK CDVaW4&'3)*5@[7`{_9]|\\"
 steps = 2
 encresult = []
@@ -15,36 +42,22 @@ while True:
 	print("   |2| Decrypt a text")
 	print("   |0| Leave the program")
 	task = input(" ")
-	
+
 	#encryption
 	if task == "1":
-		print("-------------------------------")
-		text = input(' Enter a text to encrypt:\n')
-		if text == "exit":
+		if encrypt() == 0:
 			break
-		for i in text:
-			i = chars[chars.index(i)+steps]
-			encresult.append(i)
-		print("Your encrypted text:\n",''.join(encresult))
-		print('-------------------------------\n \n \n')
 
 	#decryption
-	if task == "2":
-		print("-------------------------------")
-		text = input(' Enter a text to decrypt:\n')
-		if text == "exit":
+	elif task == "2":
+		if decrypt() == 0:
 			break
-		for i in text:
-			i = chars[chars.index(i)-steps]
-			decresult.append(i)
-		print('Your decrypted text:\n',''.join(decresult))
-		print('-------------------------------\n \n \n')
-
+	
 	#exit
-	if task == "exit" or task == "0":
+	elif task == "exit" or task == "0":
 		break
 
-	if task != "1" and task != "2":
+	else:
 		print("Invalid input, choose one of the options available.\n-------------------------------")
 
 	encresult = []
