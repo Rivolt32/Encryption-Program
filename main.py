3c2512dc2e5797eb70b581c1fb39065e3a64b121
Rivolt32 <tarik.elharim@gmail.com>
2022-04-08 17:17:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 #functions def
 def encrypt():
-	print("-------------------------------")
-	text = input(' Enter a text to encrypt:\n')
-	if text == "exit":
-		return 0
-	for i in text:
-		i = chars[chars.index(i)+steps]
-		encresult.append(i)
-	print("\nYour encrypted text:\n",''.join(encresult))
-	print('-------------------------------\n \n \n')
+    print("-------------------------------")
+    text = input(' Enter a text to encrypt:\n')
+    if text == "exit":
+        return 0
+    for i in text:
+        i = chars[chars.index(i)+steps]
+        encresult.append(i)
+    print("\nYour encrypted text:\n",''.join(encresult))
+    print('-------------------------------\n \n \n')
 
 
 def decrypt():
-	print("-------------------------------")
-	text = input(' Enter a text to decrypt:\n')
-	if text == "exit":
-		return 0
-	for i in text:
-		i = chars[chars.index(i)-steps]
-		decresult.append(i)
-	print('\nYour decrypted text:\n',''.join(decresult))
-	print('-------------------------------\n \n \n')
-	
+    print("-------------------------------")
+    text = input(' Enter a text to decrypt:\n')
+    if text == "exit":
+        return 0
+    for i in text:
+        i = chars[chars.index(i)-steps]
+        decresult.append(i)
+    print('\nYour decrypted text:\n',''.join(decresult))
+    print('-------------------------------\n \n \n')
+    
 #end of functions def
 
 
@@ -37,29 +37,29 @@ print("|           Type 'exit' anytime to leave          |")
 print("---------------------------------------------------\n")
 
 while True:
-	print(" What are you wishing to do")
-	print("   |1| Encrypt a text")
-	print("   |2| Decrypt a text")
-	print("   |0| Leave the program")
-	task = input(" ")
+    print(" What are you wishing to do")
+    print("   |1| Encrypt a text")
+    print("   |2| Decrypt a text")
+    print("   |0| Leave the program")
+    task = input(" ")
 
-	#encryption
-	if task == "1":
-		if encrypt() == 0:
-			break
+    #encryption
+    if task == "1":
+        if encrypt() == 0:
+            break
 
-	#decryption
-	elif task == "2":
-		if decrypt() == 0:
-			break
-	
-	#exit
-	elif task == "exit" or task == "0":
-		break
+    #decryption
+    elif task == "2":
+        if decrypt() == 0:
+            break
+    
+    #exit
+    elif task == "exit" or task == "0":
+        break
 
-	else:
-		print("Invalid input, choose one of the options available.\n-------------------------------")
+    else:
+        print("Invalid input, choose one of the options available.\n-------------------------------")
 
-	encresult = []
-	decresult = []
-	
+    encresult = []
+    decresult = []
+    
