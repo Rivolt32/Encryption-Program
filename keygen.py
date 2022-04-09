f88eaefa00ca76cf6d4bed7548e5f2d85cfb6f53
Rivolt32 <tarik.elharim@gmail.com>
2022-04-09 01:49:14 +0000

    Creating a keygen
    Check README.md for more info
    different chars order from main.py

---
+++ keygen.py
@@ -0,0 +1,3 @@
+# The last 3 characters of the key should stay in place
+chars = "cZà08^<qhwstuR,-defgïâz]xBEOPnüijklç{m9ro_pû%(è+HIY:}~Q7NîTUv>?6=êyb!\"#$SFXAM1ë./2;GLJK CDéV[aòW4&'3)*5@ùô`ì|\\"
+print(len(chars))
